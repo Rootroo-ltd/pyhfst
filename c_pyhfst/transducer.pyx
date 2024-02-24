f9ebc78a99794fe0fbca55f81ca0863966bcafda
Khalid Alnajjar <dr.mokha@gmail.com>
2024-02-24 23:30:17 +0200

    Updated cython implementation to apply the fixes

--- c_pyhfst/transducer.pyx
+++ c_pyhfst/transducer.pyx
@@ -21,7 +21,24 @@ cdef class Transducer:
         self.alphabet = a
         self.is_weighted = is_weighted
         self.operations = self.alphabet.operations
-        self.symbol_map = {self.alphabet.keyTable[i]:i for i in range(h.get_input_symbol_count())}
+        self.symbol_map = {}
+        self.construct_symbol_map()
         self.index_table = IndexTable(file, h.get_index_table_size())
         self.transition_table = TransitionTable(
             file, h.get_target_table_size(), is_weighted=self.is_weighted)
+
+    cpdef void construct_symbol_map(self):
+        for i in range(self.header.get_input_symbol_count()):
+            _w = self.alphabet.keyTable[i]
+            if len(_w) <= 1:
+                self.symbol_map[_w] = {None: i}
+            else:
+                _o = self.symbol_map[_w[0]]
+                for j in range(1, len(_w)):
+                    _c = _w[j]
+                    if _c not in _o:
+                        _o[_c] = {}
+                    _o = _o[_c]
+                if None in _o:
+                    raise ValueError("Duplicate symbol in symbol map")
+                _o[None] = i
